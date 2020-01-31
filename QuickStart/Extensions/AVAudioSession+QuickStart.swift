ce7aca326fcaa7e4e556bc99669a95e7e21e7b0e
Jaesung <jaesung.lee@sendbird.com>
2020-01-31 19:52:17 +0900

    Add AudioRouteChange Button

--- QuickStart/Extensions/AVAudioSession+QuickStart.swift
+++ QuickStart/Extensions/AVAudioSession+QuickStart.swift
@@ -11,7 +11,7 @@ extension AVAudioSession {
     static var `default`: AVAudioSession {
         let audioSession = AVAudioSession.sharedInstance()
         do {
-            try audioSession.setCategory(.playAndRecord)
+            try audioSession.setCategory(.playAndRecord, options: .allowBluetooth)
         } catch {}
         return audioSession
     }
