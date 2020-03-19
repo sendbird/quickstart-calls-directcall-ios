112e4c70c8d9052adddfcec4b596ebf580ed9e62
Jaesung <jaesung.lee@sendbird.com>
2020-03-19 15:44:25 +0900

    Fix UI bug and Remove `SceneDelegate`

--- QuickStart/AppDelegate.swift
+++ QuickStart/AppDelegate.swift
@@ -47,12 +47,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-    
-    // MARK: UISceneSession Lifecycle
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
 }
 
 extension AppDelegate: PKPushRegistryDelegate {
