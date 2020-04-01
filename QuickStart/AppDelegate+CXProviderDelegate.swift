76ebe121afd365382b22d8e132584fe231dd5486
damon.park <damon.park@sendbird.com>
2020-04-01 17:21:44 +0900

    add authentication while callkit transaction.

--- QuickStart/AppDelegate+CXProviderDelegate.swift
+++ QuickStart/AppDelegate+CXProviderDelegate.swift
@@ -31,31 +31,20 @@ extension AppDelegate: CXProviderDelegate {
             return
         }
         
-        // MARK: SendBirdCalls - DirectCall.accept()
-        let callOptions = CallOptions(isAudioEnabled: true, isVideoEnabled: call.isVideoCall, useFrontCamera: true)
-        let acceptParams = AcceptParams(callOptions: callOptions)
-        call.accept(with: acceptParams)
-        
-        // If there is termination: Failed to load VoiceCallViewController from Main.storyboard. Please check its storyboard ID")
-        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: call.isVideoCall ? "VideoCallViewController" : "VoiceCallViewController")
+        self.authenticateIfNeed{ [weak call] (error) in
+            guard let call = call, error == nil else {
+                action.fail()
+                return
+            }
 
-        if var dataSource = viewController as? DirectCallDataSource {
-            dataSource.call = call
-            dataSource.isDialing = false
+            // MARK: SendBirdCalls - DirectCall.accept()
+            let callOptions = CallOptions(isAudioEnabled: true, isVideoEnabled: call.isVideoCall, useFrontCamera: true)
+            let acceptParams = AcceptParams(callOptions: callOptions)
+            call.accept(with: acceptParams)
+            self.showCallController(with: call)
+            action.fulfill()
         }
-        
-        if let topViewController = UIViewController.topViewController {
-            topViewController.present(viewController, animated: true, completion: nil)
-        } else {
-            UIApplication.shared.keyWindow?.rootViewController = viewController
-            UIApplication.shared.keyWindow?.makeKeyAndVisible()
-        }
-        
-        // Signal to the system that the action has been successfully performed.
-        action.fulfill()
     }
-    
     
     func provider(_ provider: CXProvider, perform action: CXEndCallAction) {
         // Retrieve the SpeakerboxCall instance corresponding to the action's call UUID
@@ -65,9 +54,13 @@ extension AppDelegate: CXProviderDelegate {
         }
         
         // For decline
-        let params = AuthenticateParams(userId: UserDefaults.standard.user.id, accessToken: UserDefaults.standard.accessToken)
-        SendBirdCall.authenticate(with: params) { (user, error) in
-            call.end {
+        self.authenticateIfNeed{ [weak call] (error) in
+            guard error == nil else {
+                action.fail()
+                return
+            }
+            
+            call?.end {
                 action.fulfill()
             }
         }
@@ -87,3 +80,39 @@ extension AppDelegate: CXProviderDelegate {
     
     func provider(_ provider: CXProvider, timedOutPerforming action: CXAction) { }
 }
+
+extension AppDelegate {
+    
+    func authenticateIfNeed(completionHandler: @escaping (Error?) -> Void) {
+        guard SendBirdCall.currentUser == nil else {
+            completionHandler(nil)
+            return
+        }
+        
+        let params = AuthenticateParams(userId: UserDefaults.standard.user.id, accessToken: UserDefaults.standard.accessToken)
+        SendBirdCall.authenticate(with: params) { (user, error) in
+            completionHandler(error)
+        }
+    }
+}
+
+extension AppDelegate {
+    func showCallController(with call: DirectCall) {
+        // If there is termination: Failed to load VoiceCallViewController from Main.storyboard. Please check its storyboard ID")
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: call.isVideoCall ? "VideoCallViewController" : "VoiceCallViewController")
+
+        if var dataSource = viewController as? DirectCallDataSource {
+            dataSource.call = call
+            dataSource.isDialing = false
+        }
+        
+        if let topViewController = UIViewController.topViewController {
+            topViewController.present(viewController, animated: true, completion: nil)
+        } else {
+            UIApplication.shared.keyWindow?.rootViewController = viewController
+            UIApplication.shared.keyWindow?.makeKeyAndVisible()
+        }
+    }
+}
+
